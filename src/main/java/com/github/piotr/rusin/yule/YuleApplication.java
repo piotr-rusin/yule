1e4061d5ac6d49545114e06b109d58cd530fd37e
Piotr Rusin <piotr.rusin88@gmail.com>
2016-12-19 18:21:12 +0100

    Initial commit
    Project generated with Spring Tool Suite.

---
+++ src/main/java/com/github/piotr/rusin/yule/YuleApplication.java
@@ -0,0 +1,12 @@
+package com.github.piotr.rusin.yule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/piotr/rusin/yule/YuleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.piotr.rusin.yule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YuleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
