179e7cfa7c2f0ecb06cf918e0151993e6f8d6a22
Piotr Rusin <piotr.rusin88@gmail.com>
2017-01-09 12:17:37 +0100

    Rename a package
    The package com.github.piotr.rusin is being renamed to
    com.github.piotr_rusin.

--- src/main/java/com/github/piotr_rusin/yule/YuleApplication.java
+++ src/main/java/com/github/piotr_rusin/yule/YuleApplication.java
@@ -1,4 +1,4 @@
-package com.github.piotr.rusin.yule;
+package com.github.piotr_rusin.yule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
