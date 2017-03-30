4818d13418ce391130eb98f72bb3a9e2029b8f22
Piotr Rusin <piotr.rusin88@gmail.com>
2017-03-30 20:50:19 +0200

    Refactor integration tests for ArticleListener

--- src/test/java/com/github/piotr_rusin/yule/entity/ArticleListenerTests.java
+++ src/test/java/com/github/piotr_rusin/yule/entity/ArticleListenerTests.java
@@ -67,54 +67,65 @@ public class ArticleListenerTests {
 		);
 	}
 
-	private void testExpectedSlug(String expectedSlug) {
-		Article persistedArticle = entityManager.persistFlushFind(article);
-		assertThat(persistedArticle.getSlug()).isEqualTo(expectedSlug);
+	private void assertSlugIsEqualTo(String expectedSlug) {
+		assertThat(article.getSlug()).isEqualTo(expectedSlug);
+	}
+
+	private void testExpectedSlugAfterPersist(String expectedSlug) {
+		entityManager.persistFlushFind(article);
+
+		assertSlugIsEqualTo(expectedSlug);
 	}
 
 	@Test
 	public void testSlugIsCreatedFromTitle() {
 		String slugFromTitle = slg.slugify(article.getTitle());
-		testExpectedSlug(slugFromTitle);
+
+		testExpectedSlugAfterPersist(slugFromTitle);
 	}
 
 	@Test
 	public void testSlugIsPreservedWhenNotNull() {
 		String customSlug = slg.slugify("My custom slug");
 		article.setSlug(customSlug);
-		testExpectedSlug(customSlug);
+
+		testExpectedSlugAfterPersist(customSlug);
+	}
+
+	private void testExpectedSlugAfterUpdate(String expectedSlug) {
+		entityManager.flush();
+
+		assertSlugIsEqualTo(expectedSlug);
 	}
 
 	/**
 	 * Test if a slug based on the original title of a persisted
 	 * article is preserved after the title has been modified and
-	 * the change has been persisted.
+	 * the database has been updated.
 	 */
 	@Test
-	public void testSlugIsPreservedAfterUpdatingTitle() {
-		Article persistedArticle = entityManager.persistFlushFind(article);
-		String oldSlug = persistedArticle.getSlug();
-		persistedArticle.setTitle("A new title");
-		article = persistedArticle;
+	public void testSlugIsPreservedAfterUpdate() {
+		entityManager.persistAndFlush(article);
+		article.setTitle("A new title");
+		String oldSlug = article.getSlug();
 
-		testExpectedSlug(oldSlug);
+		testExpectedSlugAfterUpdate(oldSlug);
 	}
 
 	/**
 	 * Test if a new slug value is created for a persisted article
-	 * after its slug value has been set to null and the updated
-	 * article has been persisted.
+	 * after its slug value has been set to null and the database
+	 * has been updated.
 	 */
 	@Test
-	public void testSlugIsCreatedFromTitleAfterBeingUnset() {
+	public void testSlugIsCreatedFromTitleAfterUpdate() {
 		String initialSlug = slg.slugify("My custom slug");
 		article.setSlug(initialSlug);
-		Article persistedArticle = entityManager.persistFlushFind(article);
-		String newSlug = slg.slugify(persistedArticle.getTitle());
-		persistedArticle.setSlug(null);
-		article = persistedArticle;
+		entityManager.persistAndFlush(article);
+		article.setSlug(null);
+		String newSlug = slg.slugify(article.getTitle());
 
-		testExpectedSlug(newSlug);
+		testExpectedSlugAfterUpdate(newSlug);
 	}
 
 }
