cedefb972fb21afa380c9ce4bbdfbab747f92457
Piotr Rusin <piotr.rusin88@gmail.com>
2017-05-09 14:13:16 +0200

    Implement caching of previous state of an Article
    This commit adds a private "cache" field to the Article class and two
    methods: Article.cache() and Article.getCache(). The cache method is
    responsible for storing current state of the article in the "cache"
    field as a separate instance of the Article class. It is annotated as a
    post-load event handler.
    
    This way, the initial, post-load state of an article will be easily
    available.

--- src/main/java/com/github/piotr_rusin/yule/domain/Article.java
+++ src/main/java/com/github/piotr_rusin/yule/domain/Article.java
@@ -34,7 +34,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PostLoad;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.persistence.Version;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -78,6 +80,9 @@ public class Article {
 
     @Version
     private int version;
+
+    @Transient
+    private Article cache;
 
     /**
      * Specifies whether an article is also a blog post.
@@ -178,6 +183,41 @@ public class Article {
 
     public void setPost(boolean post) {
         this.post = post;
+    }
+
+    /**
+     * Save the current state of the entity into its cache object.
+     */
+    @PostLoad
+    public void cache() {
+        Article currentState = new Article(title, content);
+        currentState.creationDate = creationDate;
+        currentState.id = id;
+        currentState.modificationDate = modificationDate;
+        currentState.post = post;
+        currentState.publicationDate = publicationDate;
+        currentState.slug = slug;
+        currentState.status = status;
+        currentState.version = version;
+
+        currentState.cache = cache;
+        cache = currentState;
+    }
+
+    /**
+     * Get an Article object representing cached state of this article.
+     * <p>
+     * The cached state represents the state of this article at the
+     * moment of the last call to its {@link #cache() cache) method. It
+     * may be the state of the entity upon being loaded from database if
+     * the cache method wasn't called after this operation.
+     *
+     * @return an instance of Article representing state of the object
+     *         after the last call to the cache() method, or null if the
+     *         method wasn't called yet.
+     */
+    public Article getCache() {
+        return cache;
     }
 
     @Override
