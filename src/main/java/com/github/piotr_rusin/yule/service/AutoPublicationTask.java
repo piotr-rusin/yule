1e11dfc5af3576150a8788bd8792f52c470353db
Piotr Rusin <piotr.rusin88@gmail.com>
2017-06-02 17:17:11 +0200

    Add AutoPublicationTask class

---
+++ src/main/java/com/github/piotr_rusin/yule/service/AutoPublicationTask.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2017 Piotr Rusin <piotr.rusin88@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package com.github.piotr_rusin.yule.service;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.orm.hibernate5.HibernateOptimisticLockingFailureException;
+
+import com.github.piotr_rusin.yule.domain.Article;
+import com.github.piotr_rusin.yule.domain.ArticleStatus;
+import com.github.piotr_rusin.yule.repository.ArticleRepository;
+/**
+ * Represents an auto-publication task performed for all scheduled articles whose
+ * publication date has come or passed.
+ *
+ * @author Piotr Rusin <piotr.rusin88@gmail.com>
+ *
+ */
+public class AutoPublicationTask implements Runnable {
+    private ArticleRepository repository;
+    private Clock clock;
+    private final Logger logger;
+
+    /**
+     * Create a new instance.
+     *
+     * @param repository
+     *            is an object to be used to query for articles to
+     *            publish
+     * @param clock
+     *            is a clock object to be used for detecting delayed
+     *            executions of the task
+     * @param loggerFactory
+     */
+    public AutoPublicationTask(ArticleRepository repository, Clock clock, ILoggerFactory loggerFactory) {
+        this.repository = repository;
+        this.clock = clock;
+        logger = loggerFactory.getLogger(AutoPublicationTask.class.getName());
+    }
+
+    /**
+     * Publish all due articles, each in a separate transaction.
+     */
+    @Override
+    public void run() {
+        logger.info("Executing scheduled automatic publication task");
+        List<Article> toBePublished = repository.findCurrentAutoPublicationTargets();
+        if (toBePublished.isEmpty())
+            logger.warn("No articles to publish");
+        for (Article a : toBePublished) {
+            publish(a);
+        }
+    }
+
+    /**
+     * Publish an article.
+     * <p>
+     * If the article is being concurrently edited, the method fails and
+     * logs a warning.
+     * <p>
+     * In any case, the method calculates the difference
+     * between scheduled auto-publication time and the actual time of
+     * the attempt. If this time is longer than a minute, it may
+     * indicate some problems preventing executing the task on time
+     * (possibly server failures).
+     *
+     * @param article
+     *            is an article to be published
+     */
+    private void publish(Article article) {
+        article.setStatus(ArticleStatus.PUBLISHED);
+
+        try {
+            repository.save(article);
+            logger.info("Completed autopublication for " + article);
+        } catch (HibernateOptimisticLockingFailureException e) {
+            logger.info(String.format("Postponing auto-publication of %s due to a concurrent update", article));
+        } finally {
+            Duration delay = Duration.ofSeconds(
+                    article.getPublicationDate().until(Instant.now(clock), ChronoUnit.SECONDS));
+            if (delay.toMinutes() >= 1) {
+                logger.warn(String.format("The auto-publication attempt for %s was delayed - it was executed "
+                        + "%s after the scheduled publication time.", article, delay));
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/piotr_rusin/yule/service/AutoPublicationTaskTest.java
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2017 Piotr Rusin <piotr.rusin88@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package com.github.piotr_rusin.yule.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.api.Condition;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.springframework.orm.hibernate5.HibernateOptimisticLockingFailureException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.github.piotr_rusin.yule.domain.Article;
+import com.github.piotr_rusin.yule.domain.ArticleStatus;
+import com.github.piotr_rusin.yule.repository.ArticleRepository;
+
+import slf4jtest.Settings;
+import slf4jtest.TestLoggerFactory;
+
+@RunWith(SpringRunner.class)
+public class AutoPublicationTaskTest {
+    @Mock
+    private ArticleRepository repository;
+
+    @Mock
+    private Logger mockLogger;
+
+    private AutoPublicationTask task;
+    private List<Article> autoPublicationTargets = new ArrayList<>();
+    private Instant publicationTime;
+    private TestLoggerFactory loggerFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        setUpAutoPublicationTargetsAndTime();
+        Clock clock = Clock.fixed(publicationTime, ZoneId.systemDefault());
+        loggerFactory = Settings.instance()
+                .enableAll()
+                .delegate(AutoPublicationTask.class.getName(), mockLogger)
+                .buildLogging();
+        task = new AutoPublicationTask(repository, clock, loggerFactory);
+    }
+
+    private void setUpAutoPublicationTargetsAndTime() {
+        for (int i = 0; i < 4; i++) {
+            Article article = getAutoPublicationTarget(Instant.now());
+            if (i == 0)
+                publicationTime  = article.getPublicationDate();
+            autoPublicationTargets.add(article);
+        }
+
+        when(repository.findCurrentAutoPublicationTargets()).thenReturn(autoPublicationTargets);
+    }
+
+    private Article getAutoPublicationTarget(Instant publicationDate) {
+        int index = autoPublicationTargets.size();
+        Article article = new Article("title " + index, "content " + index);
+        article.setStatus(ArticleStatus.SCHEDULED_FOR_PUBLICATION);
+        article.setPublicationDate(publicationDate);
+        return article;
+    }
+
+    @Test
+    public void runLogsInitialMessage() {
+        task.run();
+        verify(mockLogger).info("Executing scheduled automatic publication task");
+    }
+
+    @Test
+    public void runLogsWarningIfNoArticlesToPublish() {
+        autoPublicationTargets.clear();
+        task.run();
+        verify(mockLogger).warn("No articles to publish");
+    }
+
+    @Test
+    public void runSetsPublishedStatus() {
+        task.run();
+        Condition<Article> published = new Condition<>(a -> a.getStatus() == ArticleStatus.PUBLISHED, "published");
+        assertThat(autoPublicationTargets).are(published);
+    }
+
+    @Test
+    public void runSavesArticle() {
+        task.run();
+        ArgumentCaptor<Article> article = ArgumentCaptor.forClass(Article.class);
+        verify(repository, times(autoPublicationTargets.size())).save(article.capture());
+        List<Article> articles = article.getAllValues();
+        assertThat(articles).containsOnlyElementsOf(autoPublicationTargets);
+    }
+
+    @Test
+    public void runLogsWhenOptimisticLockFails() {
+        HibernateOptimisticLockingFailureException ex = mock(HibernateOptimisticLockingFailureException.class);
+        when(repository.save(any(Article.class))).thenThrow(ex);
+        task.run();
+        for (Article a: autoPublicationTargets) {
+            verify(mockLogger).info(String.format("Postponing auto-publication of %s due to a concurrent update", a));
+        }
+    }
+
+    @Test
+    public void runLogsAnAttemptDelayedByOverAMinute() {
+        Map<Article, Duration> articleAutoPublicationDelay = new HashMap<>();
+        for (int d = 2; d <= 4; d++) {
+            Duration delay = Duration.ofSeconds(d * 60);
+            Article article = getAutoPublicationTarget(publicationTime.minus(delay));
+            autoPublicationTargets.add(article);
+            articleAutoPublicationDelay.put(article, delay);
+        }
+
+        task.run();
+        for (Map.Entry<Article, Duration> articleAndDelay: articleAutoPublicationDelay.entrySet()) {
+            Article article = articleAndDelay.getKey();
+            Duration delay = articleAndDelay.getValue();
+            verify(mockLogger).warn(String.format("The auto-publication attempt for %s was delayed - it was executed "
+                    + "%s after the scheduled publication time.", article, delay));
+        }
+    }
+}
