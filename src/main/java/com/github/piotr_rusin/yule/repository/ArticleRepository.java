b61b7ee39be47bdf5d47209ab66b4257b728ac81
Piotr Rusin <piotr.rusin88@gmail.com>
2017-08-11 20:27:11 +0200

    Add getPositionOnAdminPanelArticleList method

--- src/main/java/com/github/piotr_rusin/yule/repository/ArticleRepository.java
+++ src/main/java/com/github/piotr_rusin/yule/repository/ArticleRepository.java
@@ -49,6 +49,9 @@ public interface ArticleRepository
             + "order by a.publicationDate desc")
     List<Article> findPublishedPages();
 
+    @Query("select count(a) from Article a where a.id >= :id")
+    int getPositionOnAdminPanelArticleList(@Param("id") long id);
+
     @Query("select a from Article a where a.post = true "
             + "and a.status = com.github.piotr_rusin.yule.domain.ArticleStatus.PUBLISHED "
             + "and a.slug = :slug")
