4f113646ae5300a4d2440945432c1de93b6e1266
Piotr Rusin <piotr.rusin88@gmail.com>
2017-11-18 19:46:50 +0100

    Reorganize client side code and fix dialog errors
    This commit brings the following changes:
    * replacing dialog-polyfill based dialog windows with ones provided
    by bootstrap
    * replacing article-deletion task object and its corresponding dialog
    window with their more generic equivalents, so that they can be reused
    for implementing not only article deletion, but also other confirmable
    requests
    * instead of closing confirmation dialog after confirming a task and
    immediately opening a message dialog informing about request being sent,
    the confirmation dialog is kept open, but with a different message and
    disabled buttons.

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,10 +1,3 @@
-if (typeof HTMLDialogElement !== 'function') {
-  $('dialog').each(function() {
-    dialogPolyfill.registerDialog($(this));
-  });
-}
-
-
 (function setLocalPublicationDate() {
   console.log(
     'Setting value of local publication datetime field...');
@@ -33,38 +26,6 @@ $('#articleForm').submit(function() {
 updateTimeTags('.articleDateTime', false);
 
 
-function YuleDialog(selector) {
-  this.jqDialog = $(selector);
-}
-
-YuleDialog.prototype.showModal = function(message) {
-  this.jqDialog.find('p').text(message);
-  this.jqDialog[0].showModal();
-};
-
-YuleDialog.prototype.close = function() {
-  this.jqDialog[0].close();
-};
-
-
-var messageDialog = new YuleDialog('#messageDialog');
-
-messageDialog.showModal = function(message, hideCloseButton) {
-  var closeButton = $('#closeMessageDialog');
-  if (!hideCloseButton) {
-    closeButton.show();
-  } else {
-    closeButton.hide();
-  }
-  YuleDialog.prototype.showModal.call(this, message);
-};
-
-$('#closeMessageDialog').click(function() {
-  console.log('Closing message dialog');
-  messageDialog.close();
-});
-
-
 function ArticleIdData(jqButton) {
   this.id = jqButton.val();
   var tableRow = jqButton.closest('tr');
@@ -80,64 +41,115 @@ ArticleIdData.prototype.toString = function() {
 };
 
 
+function YuleDialog(selector) {
+  this.jqDialog = $(selector);
+}
 
-var articleDeletionRequestTask = {
-  article: null,
-  dialog: new YuleDialog('#confirmDeletion'),
-  get jqDialog() {
-    return this.dialog.jqDialog;
-  },
+YuleDialog.prototype.setMessage = function(message) {
+  this.jqDialog.find('p').text(message);
+};
 
-  init: function(jqDeleteButton) {
-    this.article = new ArticleIdData(jqDeleteButton);
-    this.dialog.showModal('Are you sure you want to remove' +
-        ' the article "' + this.article.title + '"?');
-  },
 
-  execute: function() {
-    if (this.article === null){
-      var error = 'The task wasn\'t initialized!';
-      console.log(error);
-      throw error;
-    }
-    messageDialog.showModal('Deleting the article: "' +
-        this.article.title + '"...', true);
-    var csrfToken = $("meta[name='_csrf']").attr("content");
-    if (!csrfToken){
-      csrfToken = $('input[name="_csrf"]').val();
-    }
-    console.log('Sending request for deleting ' + this.article);
-    $('<form action=/admin/article/' + this.article.id +
-        '/delete method="POST" style="display=none;">' +
-        '<input type="hidden" name="_csrf" value="' +
-        csrfToken + '"></form>').appendTo('body').submit();
+YuleDialog.prototype.show = function(message) {
+  this.setMessage(message);
+  this.jqDialog.modal('show');
+};
+
+YuleDialog.prototype.hide = function() {
+  this.jqDialog.modal('hide');
+};
+
+
+var messageDialog = new YuleDialog('#messageDialog');
+
+
+var requestConfirmationDialog = new YuleDialog('#confirm');
+
+
+requestConfirmationDialog.disableButtons = function(disabled) {
+  this.jqDialog.find('button').each(function() {
+    $(this).attr('disabled', disabled);
+  });
+};
+
+requestConfirmationDialog.show = function(message) {
+  this.disableButtons(false);
+  YuleDialog.prototype.show.call(this, message);
+};
+
+
+var task = {
+  initialMessage: null,
+  postConfirmationMessage: null,
+  description: null,
+  _execute: null,
+  dialog: requestConfirmationDialog,
+
+  init: function(params) {
+    console.log('Initializing: ' + params.description);
+    this.initialMessage = params.initialMessage;
+    this.postConfirmationMessage = params.postConfirmationMessage;
+    this.description = params.description;
+    this._execute = params.execute;
+
+    this.dialog.show(this.initialMessage);
   },
 
   cancel: function() {
-    console.log('Cancelling deletion request task for ' + this.article);
-    this.article = null;
+    console.log('Cancelling ' + this.description);
+    for (var p in this) {
+      if (this.hasOwnProperty(p)) {
+        p = null;
+      }
+    }
+  },
+
+  execute: function() {
+    console.log('Executing ' + this.description);
+    this.dialog.disableButtons(true);
+    this.dialog.setMessage(this.postConfirmationMessage);
+    this._execute();
   }
 };
 
 
 // .deleteArticle click handler (shows delete dialog)
-$('.deleteArticle').each(function(){
+$('.deleteArticle').each(function() {
   $(this).click(function(){
-    articleDeletionRequestTask.init($(this));
+    var article = new ArticleIdData($(this));
+
+    function sendDeleteArticleRequest() {
+      var csrfToken = $("meta[name='_csrf']").attr("content");
+      if (!csrfToken) {
+        csrfToken = $('input[name="_csrf"]').val();
+      }
+      console.log('Sending request for deleting ' + article);
+      $('<form action=/admin/article/' + article.id +
+        '/delete method="POST" style="display=none;">' +
+        '<input type="hidden" name="_csrf" value="' +
+        csrfToken + '"></form>').appendTo('body').submit();
+    }
+
+    task.init({
+      initialMessage: 'Are you sure you want to remove the article "' +
+      article.title + '"?',
+
+      postConfirmationMessage: 'Deleting the article: "' + article.title +
+      '"...',
+
+      description: 'Sending request to delete ' + article,
+      execute: sendDeleteArticleRequest
+    });
   });
 });
 
 
-// #deleteArticleDialog submit handler
-articleDeletionRequestTask.jqDialog.on('close', function(event, ui) {
-  console.log('Closing the deletion confirmation dialog');
-  if (this.returnValue == 'yes') {
-    console.log('Delete operation has been confirmed');
-    articleDeletionRequestTask.execute();
-  } else {
-    console.log('Delete operation has been cancelled');
-    articleDeletionRequestTask.cancel();
-  }
+$('#confirmRequest').click(function() {
+  task.execute();
+});
+
+$('#cancelRequest').click(function() {
+  task.cancel();
 });
 
 
@@ -153,4 +165,5 @@ function togglePublicationTimeHint() {
 
 togglePublicationTimeHint();
 $('#status').change(togglePublicationTimeHint);
+
 
