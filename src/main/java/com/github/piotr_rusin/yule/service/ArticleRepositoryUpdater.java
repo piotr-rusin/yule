77a1a8720852b62bf55e188d5575d43eb7b44ba7
Piotr Rusin <piotr.rusin88@gmail.com>
2017-11-29 11:50:35 +0100

    Add ArticleRepositoryUpdater class

---
+++ src/main/java/com/github/piotr_rusin/yule/service/ArticleRepositoryUpdater.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2017 Piotr Rusin <piotr.rusin88@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package com.github.piotr_rusin.yule.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+
+import com.github.piotr_rusin.yule.domain.Article;
+import com.github.piotr_rusin.yule.repository.ArticleRepository;
+
+/**
+ * A service encapsulating logic behind operations updating article repository.
+ *
+ * @author Piotr Rusin <piotr.rusin88@gmail.com>
+ */
+public class ArticleRepositoryUpdater {
+    private static final Logger logger = LoggerFactory
+            .getLogger(ArticleRepositoryUpdater.class);
+
+    private ArticleRepository articleRepository;
+    private AutoPublicationScheduler publicationScheduler;
+
+    public ArticleRepositoryUpdater(ArticleRepository articleRepository,
+            AutoPublicationScheduler publicationScheduler) {
+        this.articleRepository = articleRepository;
+        this.publicationScheduler = publicationScheduler;
+    }
+
+    /**
+     * Save a new article or update an existing one.
+     * <p>
+     * After the article is successfully saved, auto-publication task is
+     * rescheduled in case this article is scheduled for auto-publication and
+     * the next auto-publication time needs to be updated.
+     *
+     * @param article
+     *            is an article to be saved or updated.
+     * @return an article object representing the version of the article in the
+     *         database, after it was successfully saved.
+     */
+    public Article save(Article article) {
+        logger.info("Attempting to save an article: {}.", article);
+        Article saved = null;
+        try {
+            saved = articleRepository.save(article);
+            logger.info("The article {} was successfully saved.", saved);
+        } catch (ObjectOptimisticLockingFailureException e) {
+            logger.info(
+                    "The article {} was edited concurrently. The changes are "
+                            + "being merged based on an assumption that the concurrent edit "
+                            + "was only an automatic publication.",
+                    saved);
+            Article mostRecentlySaved = articleRepository
+                    .findOne(article.getId());
+            mostRecentlySaved.setAdminAlterableData(article);
+            saved = articleRepository.save(mostRecentlySaved);
+        }
+        publicationScheduler.scheduleNew();
+        return saved;
+    }
+
+    /**
+     * Delete an article from the repository.
+     * <p>
+     * After the article is successfully deleted, auto-publication task is
+     * rescheduled, in case the article was the only one scheduled for next
+     * auto-publication.
+     * 
+     * @param id
+     *            is an identifier of the article to be deleted.
+     * @return an article object representing the deleted article.
+     */
+    public Article delete(Long id) {
+        Article article = articleRepository.findOne(id);
+        articleRepository.delete(id);
+        logger.info("The article {} has been successfully deleted.", article);
+        publicationScheduler.scheduleNew();
+        return article;
+    }
+
+}
---
+++ src/main/java/com/github/piotr_rusin/yule/service/ArticleRepositoryUpdaterTest.java
@@ -0,0 +1,196 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2017 Piotr Rusin <piotr.rusin88@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package com.github.piotr_rusin.yule.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.github.piotr_rusin.yule.domain.Article;
+import com.github.piotr_rusin.yule.repository.ArticleRepository;
+
+@RunWith(SpringRunner.class)
+public class ArticleRepositoryUpdaterTest {
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @Mock
+    private AutoPublicationScheduler autoPublicationScheduler;
+
+    private ArticleRepositoryUpdater articleManager;
+
+    @Before
+    public void setUp() {
+        articleManager = new ArticleRepositoryUpdater(articleRepository,
+                autoPublicationScheduler);
+    }
+
+    private void assertSaves(Article article) {
+        verify(articleRepository).save(article);
+    }
+
+    private Article getArticleToSave() {
+        return new Article("New title", "New content");
+    }
+
+    @Test
+    public void testSaveSavesTheArticle() {
+        Article articleToSave = getArticleToSave();
+
+        articleManager.save(articleToSave);
+
+        assertSaves(articleToSave);
+    }
+
+    private void assertSchedulesAutoPublication() {
+        verify(autoPublicationScheduler).scheduleNew();
+    }
+
+    @Test
+    public void testSaveReschedulesAutoPublication() {
+        Article articleToSave = getArticleToSave();
+
+        articleManager.save(articleToSave);
+
+        assertSchedulesAutoPublication();
+    }
+
+    @Test
+    public void testSaveReturnsSavedArticle() {
+        Article articleToSave = getArticleToSave();
+        Article expected = new Article(articleToSave);
+        doReturn(expected).when(articleRepository).save(articleToSave);
+
+        Article actual = articleManager.save(articleToSave);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private Article getExistingArticle(Long id) {
+        Article existing = new Article("Existing title", "Existing content");
+        doReturn(existing).when(articleRepository).findOne(id);
+        return existing;
+    }
+
+    /**
+     * Versions of the same article involved in a concurrent update operation.
+     * 
+     * @author Piotr Rusin <piotr.rusin88@gmail.com>
+     */
+    private class ConcurrentlyUpdated {
+        /**
+         * The version of the article to be saved.
+         */
+        public Article toSave;
+        /**
+         * The newer version of the article, existing in the database at the
+         * moment of calling {@link ArticleRepositoryUpdater#save(Article)} on
+         * {@link ConcurrentlyUpdated#toSave}, as if the article is being
+         * concurrently updated.
+         */
+        public Article existing;
+
+        public ConcurrentlyUpdated() {
+            toSave = getArticleToSave();
+            doThrow(ObjectOptimisticLockingFailureException.class)
+                    .when(articleRepository).save(toSave);
+
+            existing = getExistingArticle(toSave.getId());
+        }
+    }
+
+    private ConcurrentlyUpdated getConcurrentlyUpdatedArticlePair() {
+        return new ConcurrentlyUpdated();
+    }
+
+    @Test
+    public void testSaveUpdatesConcurrentlyUpdatedArticle() {
+        ConcurrentlyUpdated pair = getConcurrentlyUpdatedArticlePair();
+
+        articleManager.save(pair.toSave);
+
+        assertThat(pair.existing).isEqualToComparingFieldByField(pair.toSave);
+    }
+
+    @Test
+    public void testSaveSavesConcurrentlyUpdatedArticle() {
+        ConcurrentlyUpdated pair = getConcurrentlyUpdatedArticlePair();
+
+        articleManager.save(pair.toSave);
+
+        assertSaves(pair.existing);
+    }
+
+    @Test
+    public void testSaveReschedulesAutoPublicationAfterSavingConcurrentlyUpdatedArticle() {
+        ConcurrentlyUpdated pair = getConcurrentlyUpdatedArticlePair();
+
+        articleManager.save(pair.toSave);
+
+        assertSchedulesAutoPublication();
+    }
+
+    @Test
+    public void testSaveReturnsConcurrentlyUpdatedArticle() {
+        ConcurrentlyUpdated pair = getConcurrentlyUpdatedArticlePair();
+
+        Article expected = new Article(pair.existing);
+        doReturn(expected).when(articleRepository).save(pair.existing);
+
+        Article actual = articleManager.save(pair.toSave);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testDeleteDeletesAnArticle() {
+        long id = 10;
+        articleManager.delete(id);
+        verify(articleRepository).delete(id);
+    }
+
+    @Test
+    public void testDeleteReschedulesAutoPublication() {
+        long id = 10;
+        articleManager.delete(id);
+        assertSchedulesAutoPublication();
+    }
+
+    @Test
+    public void testDeleteReturnsExpectedArticle() {
+        long id = 10;
+        Article expected = getExistingArticle(id);
+
+        Article actual = articleManager.delete(id);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
